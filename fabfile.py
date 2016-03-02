234e7793b21a62bf8d6386a2e32a920eba3382a6
Yuya Unno <unnonouno@gmail.com>
2016-03-02 19:04:07 +0900

    Use pyenv installer

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,8 @@ from fabric.api import cd, env, put, run, sudo, shell_env
 
 
 def install_pyenv():
-    sudo('apt-get install -yq git libbz2-dev libreadline-dev libssl-dev libsqlite-dev')
-    run('git clone https://github.com/yyuu/pyenv.git ~/.pyenv')
+    sudo('apt-get install -yq curl git libbz2-dev libreadline-dev libssl-dev libsqlite-dev')
+    run('curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash')
 
 
 def install_ubuntu():
