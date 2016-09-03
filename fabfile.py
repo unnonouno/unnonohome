7b4074496ed2e932c7d83c5e46cac4fec5a285eb
Yuya Unno <unnonouno@gmail.com>
2016-09-03 21:51:28 +0900

    Fix sqlite

--- fabfile.py
+++ fabfile.py
@@ -3,7 +3,7 @@ from fabric.api import cd, env, put, run, sudo, shell_env
 
 
 def install_pyenv():
-    sudo('apt-get install -yq curl git libbz2-dev libreadline-dev libssl-dev libsqlite-dev')
+    sudo('apt-get install -yq curl git libbz2-dev libreadline-dev libssl-dev libsqlite3-dev')
     run('curl -L https://raw.githubusercontent.com/yyuu/pyenv-installer/master/bin/pyenv-installer | bash')
 
 
